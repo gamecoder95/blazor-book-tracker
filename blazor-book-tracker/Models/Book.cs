23ce1042d5cdfbb1f414ac11e5f5960f0d187f4e
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2025-06-25 10:53:29 -0400

    Created Book model and updated the list rendering code

---
+++ blazor-book-tracker/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookTrackerAPI.Models
+{
+    public class Book
+    {
+        public string Title { get; set; } = string.Empty;
+        public bool IsRead { get; set; } = false;
+        public string Author { get; set; } = string.Empty;
+        public DateTime PublishDate { get; set; } = new();
+        public string Description { get; set; } = string.Empty;
+    }
+}
